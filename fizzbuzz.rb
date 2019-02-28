4f9f75e44017e8d18ecc3484c4e7145517c515fb
awesome-edition-6075 <zachpardee@gmail.com>
2019-02-28 09:15:54 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz(int)
     "Fizz" # Go fizz
     elsif int % 5 == 0
     "Buzz"
-    elsif int % 3 == 0 && int % 5 == 0
+    elsif (int % 3 == 0) && (int % 5 == 0)
     "FizzBuzz"
   end
 end
